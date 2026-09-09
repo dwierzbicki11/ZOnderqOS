3be4e3d2368785b646a991701c7ea30176e6635a
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-09-09 23:06:11 +0200

    Fix QEMU fullscreen option compatibility

--- run.sh
+++ run.sh
@@ -11,7 +11,8 @@ qemu-system-x86_64 \
 -boot d \
 -no-reboot \
 -no-shutdown \
--display gtk,fullscreen=on,zoom-to-fit=on \
+-display gtk,zoom-to-fit=on \
+-full-screen \
 -serial stdio \
 -device ich9-ahci,id=ahci0 \
 -drive file="zonder_disk.img",if=none,id=ahcidisk0,format=raw \
