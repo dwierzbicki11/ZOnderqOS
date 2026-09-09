4d245bc4e6fb2a16bdad7016a7e22156e73fbce6
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-09-09 22:09:11 +0200

    Add Gen3 application manager

---
+++ source/GUI/Application/ApplicationManager.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using Cosmos.Kernel.System.Keyboard;
+
+namespace ZonderqOS.GUI.Apps
+{
+    public class ApplicationManager
+    {
+        private readonly List<Application> applications = new List<Application>();
+
+        public Application ActiveApplication
+        {
+            get
+            {
+                if (applications.Count == 0) return null;
+                return applications[applications.Count - 1];
+            }
+        }
+
+        public void Launch(Application application)
+        {
+            if (application == null) return;
+            applications.Add(application);
+        }
+
+        public void Close(Application application)
+        {
+            if (application == null) return;
+            application.Close();
+            applications.Remove(application);
+        }
+
+        public void HandleKeyboard(KeyEvent key)
+        {
+            ActiveApplication?.HandleKeyboard(key);
+        }
+
+        public void Update()
+        {
+            for (int i = applications.Count - 1; i >= 0; i--)
+            {
+                if (!applications[i].IsRunning)
+                    applications.RemoveAt(i);
+                else
+                    applications[i].Update();
+            }
+        }
+
+        public void Render(Cosmos.Kernel.System.Graphics.Canvas canvas)
+        {
+            foreach (var application in applications)
+            {
+                if (application.Window != null)
+                    application.Window.Render(canvas);
+            }
+        }
+    }
+}
