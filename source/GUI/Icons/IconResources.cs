8f47907cb6c717fbb3535d56ef039aff7b808228
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-09-10 12:53:40 +0200

    Add embedded PNG icon resources

---
+++ source/GUI/Icons/IconResources.cs
@@ -0,0 +1,46 @@
+using IL2CPU.API.Attribs;
+
+namespace ZonderqOS.GUI.Icons
+{
+    internal static class IconResources
+    {
+        [ManifestResourceStream(ResourceName = "Icons.terminal-2.png")]
+        public static byte[] Terminal;
+
+        [ManifestResourceStream(ResourceName = "Icons.folder.png")]
+        public static byte[] Folder;
+
+        [ManifestResourceStream(ResourceName = "Icons.file.png")]
+        public static byte[] File;
+
+        [ManifestResourceStream(ResourceName = "Icons.settings-2.png")]
+        public static byte[] Settings;
+
+        [ManifestResourceStream(ResourceName = "Icons.info-circle.png")]
+        public static byte[] About;
+
+        [ManifestResourceStream(ResourceName = "Icons.square-rounded-x.png")]
+        public static byte[] Close;
+
+        [ManifestResourceStream(ResourceName = "Icons.arrows-maximize.png")]
+        public static byte[] Maximize;
+
+        [ManifestResourceStream(ResourceName = "Icons.restore.png")]
+        public static byte[] Restore;
+
+        [ManifestResourceStream(ResourceName = "Icons.home.png")]
+        public static byte[] Start;
+
+        [ManifestResourceStream(ResourceName = "Icons.arrow-up.png")]
+        public static byte[] ArrowUp;
+
+        [ManifestResourceStream(ResourceName = "Icons.refresh.png")]
+        public static byte[] Refresh;
+
+        [ManifestResourceStream(ResourceName = "Icons.search.png")]
+        public static byte[] Search;
+
+        [ManifestResourceStream(ResourceName = "Icons.trash.png")]
+        public static byte[] Trash;
+    }
+}
