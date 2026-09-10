9f112ea8785f58e6209ae0c1f8bfebb129ae893d
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-09-10 12:31:52 +0200

    Add GUI icon types

---
+++ source/GUI/Icons/IconType.cs
@@ -0,0 +1,16 @@
+namespace ZonderqOS.GUI.Icons
+{
+    public enum IconType
+    {
+        Terminal,
+        FileManager,
+        Settings,
+        About,
+        Folder,
+        File,
+        Close,
+        Maximize,
+        Restore,
+        Start
+    }
+}
