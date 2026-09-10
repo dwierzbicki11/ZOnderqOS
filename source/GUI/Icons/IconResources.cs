c031d23fcdf79aa18a293dd60881b74348ee0877
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-09-10 13:00:31 +0200

    Fix icon resource namespace and stream loading

--- source/GUI/Icons/IconResources.cs
+++ source/GUI/Icons/IconResources.cs
@@ -1,113 +1,49 @@
-using System.Drawing;
+using System;
 using System.IO;
-using Cosmos.Kernel.System.Graphics;
+using System.Reflection;
 
 namespace ZonderqOS.GUI.Icons
 {
-    public static class IconManager
+    internal static class IconResources
     {
-        private const int IconSize = 18;
-        private const string CacheDirectory = "/root/.zonderq-icons";
-        private static readonly Png[] imageCache = new Png[10];
+        private const string Prefix = "Icons.";
 
-        public static void Draw(Canvas canvas, IconType type, int x, int y, Color color)
+        public static byte[] Terminal { get { return Load("terminal-2.png"); } }
+        public static byte[] Folder { get { return Load("folder.png"); } }
+        public static byte[] File { get { return Load("file.png"); } }
+        public static byte[] Settings { get { return Load("settings-2.png"); } }
+        public static byte[] About { get { return Load("info-circle.png"); } }
+        public static byte[] Close { get { return Load("square-rounded-x.png"); } }
+        public static byte[] Maximize { get { return Load("arrows-maximize.png"); } }
+        public static byte[] Restore { get { return Load("restore.png"); } }
+        public static byte[] Start { get { return Load("home.png"); } }
+        public static byte[] ArrowUp { get { return Load("arrow-up.png"); } }
+        public static byte[] Refresh { get { return Load("refresh.png"); } }
+        public static byte[] Search { get { return Load("search.png"); } }
+        public static byte[] Trash { get { return Load("trash.png"); } }
+
+        private static byte[] Load(string fileName)
         {
-            Png image = GetImage(type);
-            if (image == null)
-                return;
+            Assembly assembly = typeof(IconResources).Assembly;
+            string resourceName = Prefix + fileName;
 
-            canvas.DrawImage(image, x, y, IconSize, IconSize);
-        }
-
-        private static Png GetImage(IconType type)
-        {
-            int index = (int)type;
-            if (index < 0 || index >= imageCache.Length)
-                return null;
-
-            if (imageCache[index] != null)
-                return imageCache[index];
-
-            byte[] data = GetResource(type);
-            if (data == null || data.Length == 0)
-                return null;
-
-            try
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
             {
-                Directory.CreateDirectory(CacheDirectory);
-                string path = Path.Combine(CacheDirectory, GetFileName(type));
-
-                if (!File.Exists(path))
-                    File.WriteAllBytes(path, data);
-
-                imageCache[index] = new Png(path);
-                return imageCache[index];
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
-        private static byte[] GetResource(IconType type)
-        {
-            switch (type)
-            {
-                case IconType.Terminal:
-                    return IconResources.Terminal;
-                case IconType.FileManager:
-                case IconType.Folder:
-                    return IconResources.Folder;
-                case IconType.File:
-                    return IconResources.File;
-                case IconType.Settings:
-                    return IconResources.Settings;
-                case IconType.About:
-                    return IconResources.About;
-                case IconType.Close:
-                    return IconResources.Close;
-                case IconType.Maximize:
-                    return IconResources.Maximize;
-                case IconType.Restore:
-                    return IconResources.Restore;
-                case IconType.Start:
-                    return IconResources.Start;
-                default:
+                if (stream == null || stream.Length <= 0 || stream.Length > Int32.MaxValue)
                     return null;
-            }
-        }
 
-        private static string GetFileName(IconType type)
-        {
-            switch (type)
-            {
-                case IconType.Terminal:
-                    return "terminal-2.png";
-                case IconType.FileManager:
-                case IconType.Folder:
-                    return "folder.png";
-                case IconType.File:
-                    return "file.png";
-                case IconType.Settings:
-                    return "settings-2.png";
-                case IconType.About:
-                    return "info-circle.png";
-                case IconType.Close:
-                    return "square-rounded-x.png";
-                case IconType.Maximize:
-                    return "arrows-maximize.png";
-                case IconType.Restore:
-                    return "restore.png";
-                case IconType.Start:
-                    return "home.png";
-                default:
-                    return "icon.png";
-            }
-        }
+                byte[] data = new byte[(int)stream.Length];
+                int offset = 0;
+                while (offset < data.Length)
+                {
+                    int read = stream.Read(data, offset, data.Length - offset);
+                    if (read <= 0)
+                        break;
+                    offset += read;
+                }
 
-        public static int Size
-        {
-            get { return IconSize; }
+                return offset == data.Length ? data : null;
+            }
         }
     }
 }
