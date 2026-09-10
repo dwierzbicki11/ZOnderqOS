056e9445f9b4868581bde95fe1f7f92003c90ce3
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-09-10 14:03:32 +0200

    GUI: add modern shared visual theme

---
+++ source/GUI/Theme.cs
@@ -0,0 +1,18 @@
+using System.Drawing;
+
+namespace ZonderqOS.GUI
+{
+    public static class Theme
+    {
+        public static readonly Color DesktopFallback = Color.FromArgb(14, 22, 32);
+        public static readonly Color WindowBackground = Color.FromArgb(31, 38, 48);
+        public static readonly Color WindowBorder = Color.FromArgb(72, 86, 104);
+        public static readonly Color TitleActive = Color.FromArgb(27, 82, 135);
+        public static readonly Color TitleInactive = Color.FromArgb(52, 59, 68);
+        public static readonly Color Surface = Color.FromArgb(39, 47, 58);
+        public static readonly Color SurfaceHover = Color.FromArgb(51, 62, 76);
+        public static readonly Color Accent = Color.FromArgb(55, 145, 230);
+        public static readonly Color Text = Color.FromArgb(238, 242, 247);
+        public static readonly Color MutedText = Color.FromArgb(165, 176, 190);
+    }
+}
