fd23cdef894c2c4147d04e3ac963283fe4862866
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-09-14 21:07:54 +0200

    Implement ip configuration command

--- source/Shell/Commands/CmdIp.cs
+++ source/Shell/Commands/CmdIp.cs
@@ -1,9 +1,77 @@
+using System;
+
 namespace ZonderqOS.Commands
 {
     public class CmdIp : ICommand
     {
         public string Name => "ip";
-        public string Description => "Network IP configuration";
-        public void Execute(string[] args, ref string currentPath) { }
+        public string Description => "IPv4 configuration: ip [info|dhcp|static|device|apply]";
+
+        public void Execute(string[] args, ref string currentPath)
+        {
+            string action = args.Length > 1 ? args[1].ToLower() : "info";
+
+            switch (action)
+            {
+                case "info":
+                case "show":
+                    Network.ShowInterfaceInfo();
+                    CommandIO.LastCommandSuccess = Network.ActiveDevice != null;
+                    return;
+
+                case "dhcp":
+                    SystemSettings.SetNetworkModeDhcp(true);
+                    CommandIO.LastCommandSuccess = Network.ConfigureDhcp();
+                    return;
+
+                case "static":
+                    if (args.Length < 6)
+                    {
+                        CommandIO.WriteLine("Usage: ip static <address> <mask> <gateway> <dns>");
+                        CommandIO.LastCommandSuccess = false;
+                        return;
+                    }
+
+                    if (!SystemSettings.SetStaticNetwork(args[2], args[3], args[4], args[5]))
+                    {
+                        CommandIO.WriteLine("ip: invalid static IPv4 configuration");
+                        CommandIO.LastCommandSuccess = false;
+                        return;
+                    }
+
+                    CommandIO.LastCommandSuccess = Network.ConfigureStatic(args[2], args[3], args[4], args[5]);
+                    return;
+
+                case "device":
+                    if (args.Length < 3 || !int.TryParse(args[2], out int deviceIndex))
+                    {
+                        CommandIO.WriteLine("Usage: ip device <index>");
+                        CommandIO.LastCommandSuccess = false;
+                        return;
+                    }
+
+                    CommandIO.LastCommandSuccess = Network.SetActiveDevice(deviceIndex);
+                    return;
+
+                case "apply":
+                    CommandIO.LastCommandSuccess = Network.ApplySavedConfiguration();
+                    return;
+
+                default:
+                    PrintUsage();
+                    CommandIO.LastCommandSuccess = false;
+                    return;
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            CommandIO.WriteLine("Usage:");
+            CommandIO.WriteLine("  ip info");
+            CommandIO.WriteLine("  ip dhcp");
+            CommandIO.WriteLine("  ip static <address> <mask> <gateway> <dns>");
+            CommandIO.WriteLine("  ip device <index>");
+            CommandIO.WriteLine("  ip apply");
+        }
     }
 }
